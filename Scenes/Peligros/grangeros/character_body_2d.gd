875aadf276fe01f4a7775cf178916afbc823a590
YiforFaivzd <149855485+YiforFaivzd@users.noreply.github.com>
2026-07-23 20:46:33 -0600

    IA de granjero pulida

--- Scenes/Peligros/grangeros/character_body_2d.gd
+++ Scenes/Peligros/grangeros/character_body_2d.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @export var waypoints:Array[Marker2D]
 @export var agent: NavigationAgent2D
 @export var persigueJugador:bool=false
+var is_waiting: bool = false
 const SPEED = 80.0
 const JUMP_VELOCITY = -400.0
 var current_Index:int=0
@@ -10,15 +11,23 @@ func _ready():
 	player=get_tree().get_first_node_in_group("Player")
 
 func _physics_process(delta: float) -> void:
-	var MinDistance:float=5
-
+	var direction= agent.get_next_path_position() - global_position
+	var Distance = direction.length()
+	
+	if is_waiting == true:
+		return
+	
 	if agent.is_navigation_finished():
-		current_Index+=1
+		current_Index += 1
+		velocity = Vector2.ZERO
+		$Timer2.start()
+		is_waiting = true
+		
 		if current_Index>=waypoints.size():
 			current_Index=0
 		
 	
-	var direction=agent.get_next_path_position()-global_position
+
 	velocity=direction.normalized()*SPEED
 	move_and_slide()
 
@@ -26,6 +35,11 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	if persigueJugador==false:
-			agent.target_position=waypoints[current_Index].global_position
+			var Target_position = waypoints[current_Index].global_position
+			agent.target_position=Target_position
 	if persigueJugador==true:
 		agent.target_position=player.global_position
+
+
+func _on_timer_2_timeout() -> void:
+	is_waiting = false
