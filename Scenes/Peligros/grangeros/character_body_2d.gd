e023d290a39caaf058c50506b34451d529287233
juanfelipeviteriobando <juanfelipeviterimax@gmail.com>
2026-07-23 21:09:20 -0500

    granjero persigue a jugador

--- Scenes/Peligros/grangeros/character_body_2d.gd
+++ Scenes/Peligros/grangeros/character_body_2d.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 @export var waypoints:Array[Marker2D]
 @export var agent: NavigationAgent2D
 @export var persigueJugador:bool=false
-const SPEED = 300.0
+@export var SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 var current_Index:int=0
 var player
