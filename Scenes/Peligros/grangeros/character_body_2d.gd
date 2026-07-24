ef2401a8c884873f15be00f63101dbdc4420705d
juanfelipeviteriobando <juanfelipeviterimax@gmail.com>
2026-07-24 12:14:59 -0500

    .

---
+++ Scenes/Peligros/grangeros/character_body_2d.gd
@@ -0,0 +1,72 @@
+extends CharacterBody2D
+@export var waypoints:Array[Marker2D]
+@export var agent: NavigationAgent2D
+@export var persigueJugador:bool=false
+var SPEED = 80.0
+var NormalSpeed:float = 80.0
+var Max_Speed = 120.0
+
+var current_Index:int=0
+var wait: bool = false
+var player
+func _ready():
+	player=get_tree().get_first_node_in_group("Player")
+
+func _physics_process(delta: float) -> void:
+	if wait == true:
+		return
+
+	if agent.is_navigation_finished():
+		velocity = Vector2.ZERO
+		current_Index+=1
+		if persigueJugador == false:
+			wait = true
+			$Timer2.start()
+		else:
+			wait = false
+			$Timer2.stop()
+		if current_Index>=waypoints.size():
+			current_Index=0
+		
+	
+	var direction=agent.get_next_path_position()-global_position
+	if persigueJugador == true:
+		SPEED = Max_Speed
+	else:
+		SPEED = NormalSpeed
+	velocity=direction.normalized()*SPEED
+	move_and_slide()
+
+
+
+func _on_timer_timeout() -> void:
+	if persigueJugador==false:
+		agent.target_position=waypoints[current_Index].global_position
+	if persigueJugador==true:
+		agent.target_position=player.global_position
+		
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var estado_de_espacio = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(
+	global_position,player.global_position)
+	query.exclude = [self]
+	query.collision_mask = 1 << 1 # Capa de obstáculos
+
+	var resultado = estado_de_espacio.intersect_ray(query)
+
+	if resultado.is_empty():
+		persigueJugador=true
+	else:
+		persigueJugador=false
+	
+
+
+func _on_timer_2_timeout() -> void:
+	wait = false
+
+
+func _on_area_2d_2_body_exited(body: Node2D) -> void:
+	persigueJugador=false
